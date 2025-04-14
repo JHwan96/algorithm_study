c7f30a6058a36ed1c8e2a18b6c2fabbb893d6204
JHwan96 <dlwnghks00@gmail.com>
2025-04-15 01:51:22 +0900

    BOJ 정렬 문제
    - 수 정렬하기 2750

---
+++ Java/BOJ/sort/수_정렬하기_2750.java
@@ -0,0 +1,22 @@
+package boj.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 수_정렬하기_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(num);
+        for (int i = 0; i < n; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
